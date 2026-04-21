88ffd453872946bd585140c970b701a641c9a674
MatFar <252792@edu.p.lodz.pl>
2026-04-21 16:39:49 +0200

    11.hermetyzacja nieprywatnych pól

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,7 +8,7 @@ public class DefaultCountingOutRhymer {
         return total;
     }
 
-    public int total = -1;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
@@ -35,4 +35,7 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
